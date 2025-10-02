713ea9ec1aa1af07365f74a9ab950dd809e8dc04
BoydEmmons04 <bemmons04@gmail.com>
2025-10-01 19:30:07 -0500

    Documentation Added for educational purposes, removed the scene phase env object as it was unused

--- CFA Waste/CFA_WasteApp.swift
+++ CFA Waste/CFA_WasteApp.swift
@@ -1,18 +1,17 @@
 import SwiftUI
 import Firebase
 
-@main
+@main  // defines this view to run first before anything else
 struct CFA_WasteApp: App {
-    @StateObject private var authViewModel = AuthViewModel()
+    @StateObject private var authViewModel = AuthViewModel()  // initializes the view models on open
     @StateObject private var buttonGridVM = ButtonGridViewModel()
-    @Environment(\.scenePhase) private var scenePhase
 
-    init() {
+    init() {  // Calls the firebase api to configure
         FirebaseApp.configure()
     }
 
     var body: some Scene {
-        WindowGroup {
+        WindowGroup { // defines the scene for all other views to go in
             RootView()
                 .environmentObject(authViewModel)
                 .environmentObject(buttonGridVM)
